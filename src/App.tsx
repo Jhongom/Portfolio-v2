50bede21c1879b3a40a3542f91f5fd4ae52cb7cd
Jhon Sebastian Gomez Torres <jhon.gomez-t@mail.escuelaing.edu.co>
2024-09-08 01:01:44 -0500

    :100: :Develop: New design for experience

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import Header from "./components/header/header.component";
 import Banner from "./components/banner/banner.component";
 import Skills from "./components/skills/skills.component";
 import Projects from "./components/projects/projects.component";
+import WorkExp from "./components/work-exp/WorkExp.component"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Banner></Banner>
-      <Projects></Projects>
-      <Skills></Skills>
+      <WorkExp></WorkExp>
+      
+      
 
 
     </div>
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,14 +1,11 @@
-import './header.styles.css';
-import texts from '../../assets/texts/texts.json'
-const Header = () => {
-    return (
-        <div className='header_container'>
-            <div className='header_container-elements'>
-                <p className='header_container-elements--detail'>{texts.header.title}</p>
-                <p className="header_container-elements--detail2">{texts.header.subtitle}</p>
-            </div>
-        </div>
-    );
-};
+import * as React from 'react';
+import Box from '@mui/material/Box';
 
-export default Header
+export default function BoxBasic() {
+  return (
+    <Box component="section" sx={{ p: 1, border: '1px dashed grey' }}>
+        <p>Jhon Sebastian Gomez Torres.</p>
+        <p>Full Stack Engineer.</p>
+    </Box>
+  );
+}
